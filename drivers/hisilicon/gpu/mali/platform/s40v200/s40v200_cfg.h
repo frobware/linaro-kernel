9bc911ec6b09123f0638cf0ef748244041adb5d3
Jiancheng Xue <xuejiancheng@huawei.com>
2014-06-26 16:44:09 +0800

    drivers:hisilicon: add initial version of gpu mali400
    add initial version of gpu mali400
    
    Signed-off-by: Jiancheng Xue <xuejiancheng@huawei.com>

---
+++ drivers/hisilicon/gpu/mali/platform/s40v200/s40v200_cfg.h
@@ -0,0 +1,27 @@
+#ifndef __S40V200_CFG_H__
+#define __S40V200_CFG_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define S40V200_VMIN_TEST
+
+/* #define GPU_PMU_HISI */
+
+#define GPU_DVFS_ENABLE
+
+#define GPU_AVS_SUPPORT
+#if defined (CHIP_TYPE_hi3719mv100) || defined (CHIP_TYPE_hi3718mv100)
+ #define GPU_NO_AVS_PROFILE 4   /* 300MHz */
+#else
+ #define GPU_NO_AVS_PROFILE 5   /* 250MHz */
+#endif
+
+#define GPU_DEDICATE_MEM_START 0x0
+#define GPU_DEDICATE_MEM_SIZE 0x0
+
+#ifdef __cplusplus
+}
+#endif
+#endif
