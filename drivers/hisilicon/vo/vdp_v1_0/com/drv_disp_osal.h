ab8d5b7d803dbd71ffa4a54e53310aa12af8b0f3
Jiancheng Xue <xuejiancheng@huawei.com>
2014-07-01 15:50:30 +0800

    drivers:hisilicon: resolve the compilation problems
    resolve the compilation problems
    
    Signed-off-by: Jiancheng Xue <xuejiancheng@huawei.com>

--- drivers/hisilicon/vo/vdp_v1_0/com/drv_disp_osal.h
+++ drivers/hisilicon/vo/vdp_v1_0/com/drv_disp_osal.h
@@ -62,6 +62,7 @@ History       :
 
 //for snprintf
 
+#include <mach/hardware.h>
 /*
 #include <asm/system.h>
 #include <linux/interrupt.h>
@@ -71,8 +72,8 @@ History       :
 #include <linux/time.h>
 #include <linux/unistd.h>
 #include <linux/delay.h>
-*/
 #include <mach/hardware.h>
+*/
 
 
 #elif defined(__DISP_PLATFORM_VC__)
